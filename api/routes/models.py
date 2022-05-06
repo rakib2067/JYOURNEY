8b6ac030770bea2d8514fd88588fba6142fc5d3f
dimi-fn <dimitris_fanis@protonmail.com>
2022-05-06 20:28:41 +0300

    init models, serializers, urls, views for routes

--- api/routes/models.py
+++ api/routes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Route(models.Model):
+  latitude = models.DecimalField(max_digits=9, decimal_places=6)
+  longitude = models.DecimalField(max_digits=9, decimal_places=6)
+      
+  def __str__(self):
+    return f"{self.latitude}({self.longitude})"
--- api/routes/views.py
+++ api/routes/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import RouteSerializer
+
+from .models import Route
 # Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+	api_urls = {
+		'List':'/task-list/',
+		}
+	return Response(api_urls)
+
+@api_view(['GET'])
+def routeList(request):
+	routes = Route.objects.all().order_by('-id')
+	serializer = RouteSerializer(routes, many=True)
+	return Response(serializer.data)
