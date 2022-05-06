778be5dbf202aeff03c2d5c59687f1e53cf08614
dimi-fn <dimitris_fanis@protonmail.com>
2022-05-06 13:28:46 +0300

    initialized the flask server

---
+++ server/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, jsonify
+
+# from flask_cors import CORS # enable cross origin access on all routes, from any origin
+
+
+# from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+# CORS(app) #wrap the app with the CORS module
+
+
+@app.route('/')
+def home():
+     return jsonify({'message': 'Hello from the JYOURNEY server!'}), 200
+#     return render_template('index.html')
+
+# run the app
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ server/wsgi.py
@@ -0,0 +1,8 @@
+'''
+WSGI (Web Server Gateway Interface) is an interface between web servers and web apps for python
+'''
+
+from app import app
+
+app.run()
+
