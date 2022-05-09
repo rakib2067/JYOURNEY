6bc4cb106503cf424555c49d2934977c02f66397
dimi-fn <dimitris_fanis@protonmail.com>
2022-05-09 12:56:24 +0300

    init App.js, protectedRoutes

---
+++ Client/src/protectedRoutes.jsx
@@ -0,0 +1,15 @@
+// all routes are protected apart from homepage
+import { Navigate, Outlet } from "react-router-dom";
+
+const useAuth = () => {
+  const user = { loggedIn: false };
+  return user && user.loggedIn;
+};
+
+const ProtectedRoutes = () => {
+  const isAuth = useAuth();
+  return isAuth ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default ProtectedRoutes;
+
