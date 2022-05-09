e3dcde9aeb42f35a4565caf7e6f0ab04dfd5b076
dimi-fn <dimitris_fanis@protonmail.com>
2022-05-09 19:35:54 +0300

    added favicon, configured webpack

--- Client/config/webpack.config.js
+++ Client/config/webpack.config.js
@@ -29,6 +29,7 @@ const config = {
     new HtmlWebpackPlugin({
       // used to add the JavaScript code to the HTML
       template: path.join(PUBLIC_DIRECTORY, 'index.html'),
+      favicon: './src/img/favicon.png',
     }),
   ],
   module: {
