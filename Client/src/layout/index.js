1eaf741fc64c0ffc0a69d380e5627f55a5867b7c
Rakib Ali <58184636+rakib2067@users.noreply.github.com>
2022-05-09 20:31:38 +0100

    Update index.js

--- Client/src/layout/index.js
+++ Client/src/layout/index.js
@@ -1,5 +1,5 @@
-import { Footer } from "./Footer";
-import { Header } from "./Header";
+import { Footer } from "./footer";
+import { Header } from "./header";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
