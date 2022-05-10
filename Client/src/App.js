adbd83239deea84491238e281f82535c43e4da9c
dimi-fn <dimitris_fanis@protonmail.com>
2022-05-10 19:00:13 +0300

    buttons for view feed and save route

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Account from "./components/Account";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import "./styles/globals.css";
 import {
--- Client/src/pages/Home/index.js
+++ Client/src/pages/Home/index.js
@@ -5,7 +5,12 @@ import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import "./index.css";
 import { Map } from "../../components/Map";
 
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from "react-router-dom";
+
+
 export const Home = () => {
+  const goTo = useNavigate();
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyBo05dfuypv2g57ktjFAyPAYGe-5Nv4h_0",
     libraries: ["places"],
@@ -15,8 +20,23 @@ export const Home = () => {
   return (
     <>
       <Map />
-      <h1>(Protected) Home Page</h1>
+      <div className="d-grid gap-2">
+
+      <Button variant="secondary" size="lg" onClick={() => goTo('/UserDetails')} style={{cursor: 'pointer'}}>
+        Save Route
+      </Button>
+
+      <h1>Latest Posts</h1>
+
+     
+      <Button variant="primary" size="lg"  onClick={() => goTo('/Feed')} style={{cursor: 'pointer'}}>
+        View Feed Page
+      </Button>
+      
+    </div>
     </>
+
+
   );
 };
 
