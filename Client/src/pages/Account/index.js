404658a040d7890006a668b2ccb59aa2f2e0c0e5
Marco <marcoporcaro87@gmail.com>
2022-05-11 12:41:13 +0100

    Co-authored-by: Leanne <leannesobogun@users.noreply.github.com> Co-authored-by: Rakib Ali <rakib2067@users.noreply.github.com>

--- Client/src/pages/Account/index.js
+++ Client/src/pages/Account/index.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { ToggleButton } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 
 import "./index.css"
 
@@ -40,6 +41,56 @@ export function Account() {
     );
   }
 
+  function Modal() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+
+
+
   return (
     <>
       <h1 fontSize="6xl"> My Journeys</h1>
@@ -50,8 +101,10 @@ export function Account() {
               <Card.Body>
                 <Card.Title>{route.route_title}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{route.post_date}</Card.Subtitle>
+                <Card.Text>Distance: {route.distance}</Card.Text>
+                <Card.Text>Duration: {route.duration}</Card.Text>
                 <div className="btn-container">
-                  <Button variant="primary">Post the route</Button>
+                  <Button variant="primary" onClick={Modal}>Post the route</Button>
                   <Checkbox />
                 </div>
               </Card.Body>
