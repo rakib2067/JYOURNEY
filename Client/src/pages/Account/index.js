262c4dfa5cf5ac98a372e39ecc118813e7ac51d9
Marco <marcoporcaro87@gmail.com>
2022-05-11 14:12:07 +0100

    adding modal

--- Client/src/pages/Account/index.js
+++ Client/src/pages/Account/index.js
@@ -4,11 +4,16 @@ import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { ToggleButton } from "react-bootstrap";
 import { Modal } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 import "./index.css"
 
 export function Account() {
   let [routes, setRoutes] = useState();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   useEffect(() => {
     getRoutes();
   }, []);
@@ -41,56 +46,6 @@ export function Account() {
     );
   }
 
-  function Modal() {
-    const [show, setShow] = useState(false);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    return (
-      <>
-        <Button variant="primary" onClick={handleShow}>
-          Launch demo modal
-        </Button>
-  
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="name@example.com"
-                  autoFocus
-                />
-              </Form.Group>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label>Example textarea</Form.Label>
-                <Form.Control as="textarea" rows={3} />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
-
-
-
   return (
     <>
       <h1 fontSize="6xl"> My Journeys</h1>
@@ -104,7 +59,41 @@ export function Account() {
                 <Card.Text>Distance: {route.distance}</Card.Text>
                 <Card.Text>Duration: {route.duration}</Card.Text>
                 <div className="btn-container">
-                  <Button variant="primary" onClick={Modal}>Post the route</Button>
+                  <Button variant="primary" onClick={handleShow}>Post the route</Button>
+
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                          <Form.Label>Email address</Form.Label>
+                          <Form.Control
+                            type="email"
+                            placeholder="name@example.com"
+                            autoFocus
+                          />
+                        </Form.Group>
+                        <Form.Group
+                          className="mb-3"
+                          controlId="exampleForm.ControlTextarea1"
+                        >
+                          <Form.Label>Example textarea</Form.Label>
+                          <Form.Control as="textarea" rows={3} />
+                        </Form.Group>
+                      </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        Close
+                      </Button>
+                      <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+
                   <Checkbox />
                 </div>
               </Card.Body>
@@ -112,7 +101,6 @@ export function Account() {
           </div>
         </>
       )}
-
     </>
   );
 }
