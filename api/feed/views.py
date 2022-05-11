1f874007c03ad556942ccb46f564bb805aabc6c8
rakib2067 <rakib7@hotmail.co.uk>
2022-05-11 17:10:03 +0100

    fixed old merge of comments endpoints

--- api/feed/views.py
+++ api/feed/views.py
@@ -6,9 +6,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
 from rest_framework.response import Response
-from .serializers import PostSerializer,GetPostSerializer
+from .serializers import PostSerializer,GetPostSerializer,CommentSerializer,CreateCommentSerializer
 
-from .models import Post
+from .models import Post,Comment
 
 
 @api_view(['GET'])
@@ -88,4 +88,72 @@ def delete_view(request,id):
         return Response(data=data)	
         
 
-	
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def get_comments(request, id):
+    try:
+        post = get_object_or_404(Post, id=id)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        comments = Comment.objects.filter(post=post)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def post_comment(request):
+
+    account = request.user
+    comment = Comment(name=account)
+
+    if request.method == 'POST':
+        serializer = CreateCommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def delete_comment(request, id):
+
+    try:
+        account = request.user
+        comment = Comment.objects.get(pk=id)
+    except Comment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        if comment.name != account:
+            return Response({'response': "You don't have permission to delete this comment!"}, status=status.HTTP_403_FORBIDDEN)
+        operation = comment.delete()
+        data = {}
+        if operation:
+            data['success'] = "delete succesful"
+        else:
+            data["failure"] = "delete failed"
+        return Response(data=data)
+
+
+@api_view(['PUT'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def like_comment(request, id):
+    try:
+        account = request.user
+        comment = Comment.objects.get(pk=id)
+        if comment.likes.filter(id=account.id).exists():
+            comment.likes.remove(account)
+        else:
+            comment.likes.add(account)
+    except Comment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    comment.save()
+    return Response(status=status.HTTP_204_NO_CONTENT)
