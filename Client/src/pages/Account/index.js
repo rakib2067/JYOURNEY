17a9d2329795ea49a1556e69fcbb1a5df107e8c0
rakib2067 <rakib7@hotmail.co.uk>
2022-05-11 14:17:46 +0100

    merged with Leanna branch, modals working

--- Client/src/pages/Account/index.js
+++ Client/src/pages/Account/index.js
@@ -3,11 +3,15 @@ import axios from "axios";
 import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { ToggleButton } from "react-bootstrap";
-
-import "./index.css"
+import { Modal } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import "./index.css";
 
 export function Account() {
-  let [routes, setRoutes] = useState();
+  const [routes, setRoutes] = useState();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   useEffect(() => {
     getRoutes();
   }, []);
@@ -43,25 +47,66 @@ export function Account() {
   return (
     <>
       <h1 fontSize="6xl"> My Journeys</h1>
-      {routes && routes.map((route) =>
-        <>
-          <div className="card-container">
-            <Card style={{ width: '18rem' }}>
-              <Card.Body>
-                <Card.Title>{route.route_title}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{route.post_date}</Card.Subtitle>
-                <div className="btn-container">
-                  <Button variant="primary">Post the route</Button>
-                  <Checkbox />
-                </div>
-              </Card.Body>
-            </Card>
-          </div>
-        </>
-      )}
+      {routes &&
+        routes.map((route) => (
+          <>
+            <div className="card-container">
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>{route.route_title}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {route.post_date}
+                  </Card.Subtitle>
+                  <Card.Text>Distance: {route.distance}</Card.Text>
+                  <Card.Text>Duration: {route.duration}</Card.Text>
+                  <div className="btn-container">
+                    <Button variant="primary" onClick={handleShow}>
+                      Post the route
+                    </Button>
 
+                    <Modal show={show} onHide={handleClose}>
+                      <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body>
+                        <Form>
+                          <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlInput1"
+                          >
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                              type="email"
+                              placeholder="name@example.com"
+                              autoFocus
+                            />
+                          </Form.Group>
+                          <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlTextarea1"
+                          >
+                            <Form.Label>Example textarea</Form.Label>
+                            <Form.Control as="textarea" rows={3} />
+                          </Form.Group>
+                        </Form>
+                      </Modal.Body>
+                      <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                          Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                          Save Changes
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
+
+                    <Checkbox />
+                  </div>
+                </Card.Body>
+              </Card>
+            </div>
+          </>
+        ))}
     </>
   );
 }
-
-
